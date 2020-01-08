baa080d203cbf5f016c0a483679952c5bb76a951
alejandra010 <52213314+alejandra010@users.noreply.github.com>
2020-01-08 08:16:31 -0500

    Update practica.cpp
    libreria

--- practica.cpp
+++ practica.cpp
@@ -1,5 +1,7 @@
-#include <stdio.h> #include <stdlib.h>
-#include <pthread.h> //crear funciones #include <unistd.h> // libreria para sleep #include<windows.h>
+#include <stdio.h> 
+#include <stdlib.h>
+#include <pthread.h> //crear funciones 
+#include <unistd.h> // libreria para sleep 
+#include<windows.h>
 #include <string.h>
-
 
