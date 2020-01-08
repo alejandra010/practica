e46d15f72ae756a1a4fa7bf272bb9f9dedb00e42
Katherine1612 <52213235+Katherine1612@users.noreply.github.com>
2020-01-08 08:23:28 -0500

    Update practica.cpp
    segunda

--- practica.cpp
+++ practica.cpp
@@ -5,3 +5,26 @@
 #include<windows.h>
 #include <string.h>
 
+void *verde(void*); 
+void *naranja(void*);
+void *rojo(void*);
+
+void *verde(void *tid)
+{
+system("cls"); //cls o clear es para limpiar la pantalla de comandos 
+system("color 20");
+Sleep(3000);
+
+}
+
+void *naranja(void *tid)
+{
+system("cls"); 
+system("color E0"); 
+Sleep(3000);
+}
+void *rojo(void *tid)
+{
+system("cls"); system("color 40"); Sleep(3000);
+}
+
