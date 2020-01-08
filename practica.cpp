2802744a5afc883d37d0ca7ab8ee782424e28df4
stalin1998 <52326274+stalin1998@users.noreply.github.com>
2020-01-08 08:27:17 -0500

    Update practica.cpp
    tercero

--- practica.cpp
+++ practica.cpp
@@ -27,4 +27,20 @@ void *rojo(void *tid)
 {
 system("cls"); system("color 40"); Sleep(3000);
 }
+int main()
+{
+int i = 1;
+pthread_t pthread_rojo, pthread_naranja, pthread_verde;
+
+pthread_create (&pthread_verde, NULL, verde, (void *)i); pthread_join(pthread_verde, NULL);
+
+pthread_create (&pthread_naranja,NULL, naranja, (void *)i); pthread_join(pthread_naranja, NULL);
+
+pthread_create (&pthread_rojo, NULL, rojo, (void *)i); pthread_join(pthread_rojo, NULL);
+
+pthread_exit(NULL);
+
+return 0;
+}
+
 
